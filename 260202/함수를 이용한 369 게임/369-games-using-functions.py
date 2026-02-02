1705204adb4843a752f79f2a937ea99625478da3
이윤우_구미_1반 <167078198+luster-woo@users.noreply.github.com>
2026-02-02 20:44:35 +0900

    Title: 함수를 이용한 369 게임, Time: 64ms, Memory: 17MB, Status: Passed - Codetree

--- 260202/함수를 이용한 369 게임/369-games-using-functions.py
+++ 260202/함수를 이용한 369 게임/369-games-using-functions.py
@@ -2,9 +2,11 @@ a, b = map(int, input().split())
 cnt = 0
 
 for i in range(a, b+1):
-    c = i % 10
-    d = i // 10
-    if i % 3 == 0 or c in [3,6,9] or d in [3,6,9]:
+    if i % 3 == 0:
         cnt += 1
+    else:
+        s = str(i)
+        if '3' in s or '6' in s or '9' in s:
+            cnt += 1
 
 print(cnt)
