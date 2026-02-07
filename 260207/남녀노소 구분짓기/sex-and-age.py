3bafc076bb94b6b26facf10ed078a042068f52c1
이윤우_구미_1반 <167078198+luster-woo@users.noreply.github.com>
2026-02-07 20:29:49 +0900

    Title: 남녀노소 구분짓기, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

--- 260207/남녀노소 구분짓기/sex-and-age.py
+++ 260207/남녀노소 구분짓기/sex-and-age.py
@@ -1,12 +1,12 @@
 a = int(input())
 b = int(input())
 if a==0:
-    if b<=19:
+    if b<19:
         print("BOY")
     else:
         print("MAN")
 else:
-    if b<=19:
+    if b<19:
         print("GIRL")
     else:
         print("WOMAN")
