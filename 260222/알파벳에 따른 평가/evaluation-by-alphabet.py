a752852cab2b26b27fb325f9ba28727d2467a5ac
이윤우_구미_1반 <167078198+luster-woo@users.noreply.github.com>
2026-02-22 07:33:10 +0900

    Title: 알파벳에 따른 평가, Time: 45ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 260222/알파벳에 따른 평가/evaluation-by-alphabet.py
@@ -0,0 +1,11 @@
+a = input()
+if a=="S":
+    print("Superior")
+elif a=="A":
+    print("Excellent")
+elif a=="B":
+    print("Good")
+elif a=="C":
+    print("Usually")
+else:
+    print("Effort")
