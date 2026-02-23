61096d238be3b044f62103979ca2bc34d3ace816
이윤우_구미_1반 <167078198+luster-woo@users.noreply.github.com>
2026-02-23 20:15:39 +0900

    Title: 4가지 관계연산자, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 260223/4가지 관계연산자/four-relational-operators.py
@@ -0,0 +1,17 @@
+a,b = map(int,input().split())
+if a>=b:
+    print(1)
+else:
+    print(0)
+if a>b:
+    print(1)
+else:
+    print(0)
+if b>=a:
+    print(1)
+else:
+    print(0)
+if b>a:
+    print(1)
+else:
+    print(0)
