c2762a90df59a2657fa896b83c08cf095d763faf
이윤우_구미_1반 <167078198+luster-woo@users.noreply.github.com>
2026-02-24 17:45:20 +0900

    Title: 두 사람, Time: 47ms, Memory: 16MB, Status: Passed - Codetree

--- 260224/두 사람/two-person.py
+++ 260224/두 사람/two-person.py
@@ -1,7 +1,8 @@
 arr =list(input().split())
-arr_1 =list(input())
+arr_1 =list(input().split())
 if (int(arr[0])>=19 and arr[1]=="M") or(int(arr_1[0])>=19 and arr_1[1]=="M"):
     print(1)
+
 else:
     print(0)
 
