39004368ee5deeacd845914acf252e4648bf2af2
이윤우_구미_1반 <167078198+luster-woo@users.noreply.github.com>
2026-03-07 20:54:44 +0900

    Title: 정상적인 구구단, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

--- 260307/정상적인 구구단/normal-multiple.py
+++ 260307/정상적인 구구단/normal-multiple.py
@@ -1,7 +1,7 @@
 n = int(input())
 for x in range(1,n+1):
     for y in range(1,n+1):
-        if y ==3:
+        if y == n:
             print(f'{x} * {y} = {x*y}')
         else:           
             print(f'{x} * {y} = {x*y},',end=" ")
