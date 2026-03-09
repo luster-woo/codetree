1358e46c726c06fb8eb8c9225b123720f07e43bd
이윤우_구미_1반 <167078198+luster-woo@users.noreply.github.com>
2026-03-09 21:22:06 +0900

    Title: 특정 수 도달하기, Time: 43ms, Memory: 16MB, Status: Passed - Codetree

--- 260309/특정 수 도달하기/reaching-specific-number.py
+++ 260309/특정 수 도달하기/reaching-specific-number.py
@@ -1,9 +1,13 @@
 
 arr = list(map(int,input().split()))
-idx = 0
+idx = -1
 for x in arr:
     if x >=250:
         idx = arr.index(x)
         break
-result = sum(arr[:idx])
-print(f'{result} {result/idx:0.1f}')
+if idx!=-1:
+    result = sum(arr[:idx])
+    print(f'{result} {result/idx:0.1f}')
+else:
+    result = sum(arr)
+    print(f'{result} {result/10:0.1f}')
