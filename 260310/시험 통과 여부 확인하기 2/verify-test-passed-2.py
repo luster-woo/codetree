77a8f08765ef728b90b572e367c4e265ad52d965
이윤우_구미_1반 <167078198+luster-woo@users.noreply.github.com>
2026-03-10 22:50:37 +0900

    Title: 시험 통과 여부 확인하기 2, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 260310/시험 통과 여부 확인하기 2/verify-test-passed-2.py
@@ -0,0 +1,10 @@
+n = int(input())
+cnt = 0
+for _ in range(n):
+    arr = list(map(int,input().split()))
+    if sum(arr)/4>=60:
+        cnt+=1
+        print('pass')
+    else:
+        print('fail')
+print(cnt)
