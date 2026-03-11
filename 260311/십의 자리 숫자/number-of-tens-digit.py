c1e4eb8a5adb1aac66347771b9f36612c20496ee
이윤우_구미_1반 <167078198+luster-woo@users.noreply.github.com>
2026-03-11 22:37:37 +0900

    Title: 십의 자리 숫자, Time: 48ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 260311/십의 자리 숫자/number-of-tens-digit.py
+++ 260311/십의 자리 숫자/number-of-tens-digit.py
@@ -1,8 +1,8 @@
-arr =list(map(int,input().split()))
-arr.pop()
+arr = list(map(int,input().split()))
+
 for i in range(1,10):
     cnt = 0
     for x in arr:
-        if x//10 == i:
-            cnt+=1
+        if x != 0 and x//10 == i:
+            cnt += 1
     print(f'{i} - {cnt}')
