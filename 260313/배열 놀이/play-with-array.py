d70b39e5cab6038e76739ec27bd0f94dfa3055ce
이윤우_구미_1반 <167078198+luster-woo@users.noreply.github.com>
2026-03-13 11:03:51 +0900

    Title: 배열 놀이, Time: 47ms, Memory: 16MB, Status: Passed - Codetree

--- 260313/배열 놀이/play-with-array.py
+++ 260313/배열 놀이/play-with-array.py
@@ -1,6 +1,6 @@
 n,q = map(int,input().split())
 arr = list(map(int,input().split()))
-for _ in range(ㅂ):
+for _ in range(q):
     temp = list(map(int,input().split()))
     if temp[0]==1:
         print(arr[temp[1]-1])
