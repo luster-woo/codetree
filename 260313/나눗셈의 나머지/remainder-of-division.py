6db0e52b2bfd7472fe3fa7e6481bff69718b5468
이윤우_구미_1반 <167078198+luster-woo@users.noreply.github.com>
2026-03-13 10:43:07 +0900

    Title: 나눗셈의 나머지, Time: 47ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 260313/나눗셈의 나머지/remainder-of-division.py
+++ 260313/나눗셈의 나머지/remainder-of-division.py
@@ -1,13 +1,10 @@
 a, b = map(int, input().split())
-count = [0] * 10
-
-# a가 0이 될 때까지 반복하며 b로 나눈 나머지를 구함
+count = [0] * b
 while a > 0:
     count[a % b] += 1
     a //= b
-
 result = 0
-for i in range(10):
+for i in range(b):
     if count[i] != 0:
         result += count[i] ** 2
 print(result)
