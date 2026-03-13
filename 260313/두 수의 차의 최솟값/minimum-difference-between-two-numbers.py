730766cc87cc8c66cfacb4d7892cf713659bab5b
이윤우_구미_1반 <167078198+luster-woo@users.noreply.github.com>
2026-03-13 14:42:38 +0900

    Title: 두 수의 차의 최솟값, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

--- 260313/두 수의 차의 최솟값/minimum-difference-between-two-numbers.py
+++ 260313/두 수의 차의 최솟값/minimum-difference-between-two-numbers.py
@@ -3,7 +3,7 @@ n = int(input())
 arr = list(map(int,input().split()))
 min_val = 10**9
 for x in range(n):
-    for y in range(x,n):
+    for y in range(x+1,n):
         if abs(arr[y]-arr[x])<min_val:
             min_val = abs(arr[y]-arr[x])
 print(min_val)
