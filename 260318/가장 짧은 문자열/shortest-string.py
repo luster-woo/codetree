5b762e74e380297789ea2f2839f9fc6d6072387d
이윤우_구미_1반 <167078198+luster-woo@users.noreply.github.com>
2026-03-18 23:30:26 +0900

    Title: 가장 짧은 문자열, Time: 78ms, Memory: 19MB, Status: Runtime Error - Codetree

--- 260318/가장 짧은 문자열/shortest-string.py
+++ 260318/가장 짧은 문자열/shortest-string.py
@@ -1,5 +1,7 @@
 min_val = 10**9
+max_val = 0
 for _ in range(3):
     temp = input()
     min_val = min(min_val, len(temp))
-print(min_val)
+    max_val = max(max_val, len(temp))
+print(max_vla - min_val)
