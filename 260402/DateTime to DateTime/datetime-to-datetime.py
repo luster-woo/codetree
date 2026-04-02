f93d06b761fb1cb9f6130b3c46935064727c0dd3
이윤우_구미_1반 <167078198+luster-woo@users.noreply.github.com>
2026-04-02 13:54:55 +0900

    Title: DateTime to DateTime, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260402/DateTime to DateTime/datetime-to-datetime.py
+++ 260402/DateTime to DateTime/datetime-to-datetime.py
@@ -1,5 +1,8 @@
 a, b, c = map(int, input().split())
 result = 11*24*60+11*60+11
 temp  = a*24*60+b*60+c
-print(temp-result)
+if temp>=result:
+    print(temp-result)
+else:
+    print(-1)
 # Please write your code here.
