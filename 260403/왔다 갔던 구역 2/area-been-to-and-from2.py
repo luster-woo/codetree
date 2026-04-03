58e66b385228a015fa7aa659b3e57ef421222d69
이윤우_구미_1반 <167078198+luster-woo@users.noreply.github.com>
2026-04-03 14:45:47 +0900

    Title: 왔다 갔던 구역 2, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 260403/왔다 갔던 구역 2/area-been-to-and-from2.py
+++ 260403/왔다 갔던 구역 2/area-been-to-and-from2.py
@@ -1,20 +1,26 @@
 import sys
 input = sys.stdin.readline
+
 n = int(input())
-arr = [0]*1000
-start = 500
+arr = [0]*2001  # 넉넉하게
+start = 1000    # 가운데 시작
+
 for _ in range(n):
-    temp  = list(map(str,input().split()))
-    if temp[1]=="R":
-        for x in range(int(temp[0])):
-            arr[start+x]+=1
-        start+=int(temp[0])
+    dist, dir = input().split()
+    dist = int(dist)
+
+    if dir == "R":
+        for x in range(1, dist+1):   # 1부터!
+            arr[start + x] += 1
+        start += dist
     else:
-        for x in range(int(temp[0])):
-            arr[start-x]+=1
-        start-=int(temp[0])
+        for x in range(1, dist+1):   # 1부터!
+            arr[start - x] += 1
+        start -= dist
+
 cnt = 0
 for x in arr:
-    if x>=2:
-        cnt+=1
+    if x >= 2:
+        cnt += 1
+
 print(cnt)
