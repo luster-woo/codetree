ede8e6a5e472f17b9263b61886d591782434536c
이윤우_구미_1반 <167078198+luster-woo@users.noreply.github.com>
2026-04-03 11:23:16 +0900

    Title: 최대로 겹치는 지점, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260403/최대로 겹치는 지점/maximum-overlapped-points.py
@@ -0,0 +1,22 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+events = []
+
+for _ in range(n):
+    x1, x2 = map(int, input().split())
+    events.append((x1, 1))   # 시작
+    events.append((x2, -1))  # 끝
+
+# 정렬: 좌표 같으면 시작(+1)이 먼저!
+events.sort(key=lambda x: (x[0], -x[1]))
+
+cur = 0
+max_val = 0
+
+for x, val in events:
+    cur += val
+    max_val = max(max_val, cur)
+
+print(max_val)
