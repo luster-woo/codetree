9b3d011d5ba94b5f64c41e66dad5c4fe61b0823f
이윤우_구미_1반 <167078198+luster-woo@users.noreply.github.com>
2026-04-03 11:22:03 +0900

    Title: 최대로 겹치는 구간, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260403/최대로 겹치는 구간/maximum-overlapped-segments.py
+++ 260403/최대로 겹치는 구간/maximum-overlapped-segments.py
@@ -1,7 +1,22 @@
-k = int(input())
-arr = [0]*100
-for _ in range(k):
-    a,b = map(int,input().split())
-    for x in range(a-1,b):
-        arr[x]+=1
-print(max(arr))
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+events = []
+
+for _ in range(n):
+    x1, x2 = map(int, input().split())
+    events.append((x1, 1))   # 시작
+    events.append((x2, -1))  # 끝
+
+# 정렬: 좌표 기준, 같으면 끝(-1)이 먼저
+events.sort(key=lambda x: (x[0], x[1]))
+
+cur = 0
+max_val = 0
+
+for x, val in events:
+    cur += val
+    max_val = max(max_val, cur)
+
+print(max_val)
