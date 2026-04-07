34b8a5bd8c0b1a4314de7d4a0ae394959dcc10a8
이윤우_구미_1반 <167078198+luster-woo@users.noreply.github.com>
2026-04-07 15:32:59 +0900

    Title: 연속되는 수 4, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260407/연속되는 수 4/continuous-number4.py
@@ -0,0 +1,15 @@
+n = int(input())
+arr = []
+for _ in range(n):
+    temp = int(input())
+    arr.append(temp)
+max_val = 0
+temp = 1
+for x in range(len(arr)-1):
+    if arr[x]>=arr[x+1]:
+        max_val = max(max_val,temp)
+        temp = 1
+    else:
+        temp+=1
+max_val = max(max_val,temp)
+print(max_val)
