83d98da0c5592d02d39dda4e1f4a0f79d1523686
이윤우_구미_1반 <167078198+luster-woo@users.noreply.github.com>
2026-04-08 12:07:04 +0900

    Title: 벌금은 누구에게, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 260408/벌금은 누구에게/who-will-pay.py
+++ 260408/벌금은 누구에게/who-will-pay.py
@@ -3,8 +3,8 @@ arr = [0]*n
 for x in range(m):
     temp = int(input())
     arr[temp-1]+=1
-for x in range(n):
-    if arr[x]>=k:
+    if arr[temp-1]>=k:
         print(x+1)
         exit()
+
 print(-1)
