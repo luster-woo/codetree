d3091290377d3b1eb7b01ff5ff000a9f08fa77a1
이윤우_구미_1반 <167078198+luster-woo@users.noreply.github.com>
2026-04-13 21:31:08 +0900

    Title: 문자에 따른 명령 2, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260413/문자에 따른 명령 2/text-based-commands2.py
+++ 260413/문자에 따른 명령 2/text-based-commands2.py
@@ -1,12 +1,18 @@
 dirs = input()
-dx = [0,1,0,-1]
-dy = [1,0,-1,0]
-i = 2
-for x in dirs:
-    if x=="L":
-        i = (i+1)%4
-    elif x=="R":
-        i = (i-1)%4
-    else:
-        print(dx[i],dy[i])
-# Please write your code here.
+
+dx = [0, 1, 0, -1]
+dy = [1, 0, -1, 0]
+
+i = 0  # 북쪽 시작
+x, y = 0, 0
+
+for d in dirs:
+    if d == "L":
+        i = (i - 1) % 4
+    elif d == "R":
+        i = (i + 1) % 4
+    else:  # F
+        x += dx[i]
+        y += dy[i]
+
+print(x, y)
