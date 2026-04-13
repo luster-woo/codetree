f3005585c52651b8397027db8ca64db8a1e57a72
이윤우_구미_1반 <167078198+luster-woo@users.noreply.github.com>
2026-04-13 21:14:56 +0900

    Title: 악수와 전염병의 상관관계 2, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260413/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
@@ -0,0 +1,18 @@
+n,k,p,t = map(int,input().split())
+arr = []
+for _ in range(t):
+    t, x, y = map(int, input().split())
+    arr.append((t, x, y))
+arr.sort(key = lambda x : x[0])
+people = [k]*n
+result = [0]*n
+result[p-1]=1
+for _,x,y in arr:
+    if people[x-1]>0 and result[x-1]==1:
+        result[y-1]=1
+        people[y-1]-=1
+    if people[y-1]>0 and result[y-1]==1:
+        people[x-1]-=1
+        result[x-1]=1
+for x in range(n):
+    print(result[x],end="")
