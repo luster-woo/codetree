eba38aca38b32a625c32b43c16e9486f482e79e8
이윤우_구미_1반 <167078198+luster-woo@users.noreply.github.com>
2026-04-13 21:16:24 +0900

    Title: 악수와 전염병의 상관관계 2, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 260413/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
+++ 260413/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
@@ -1,37 +1,19 @@
-n, k, p, t = map(int, input().split())
 
+n,k,p,t = map(int,input().split())
 arr = []
 for _ in range(t):
-    time, x, y = map(int, input().split())
-    arr.append((time, x, y))
-
-arr.sort(key=lambda x: x[0])
-
-infected = [0] * n
-remain = [0] * n
-
-infected[p-1] = 1
-remain[p-1] = k
-
-for _, x, y in arr:
-    x -= 1
-    y -= 1
-
-    # 현재 상태 백업
-    x_inf = infected[x]
-    y_inf = infected[y]
-
-    # 전염
-    if x_inf and remain[x] > 0:
-        infected[y] = 1
-    if y_inf and remain[y] > 0:
-        infected[x] = 1
-
-    # 횟수 감소 (감염자면 무조건 시도)
-    if x_inf and remain[x] > 0:
-        remain[x] -= 1
-    if y_inf and remain[y] > 0:
-        remain[y] -= 1
-
-for i in range(n):
-    print(infected[i], end="")
+    t, x, y = map(int, input().split())
+    arr.append((t, x, y))
+arr.sort(key = lambda x : x[0])
+people = [k]*n
+result = [0]*n
+result[p-1]=1
+for _,x,y in arr:
+    if people[x-1]>0 and result[x-1]==1:
+        result[y-1]=1
+        people[x-1]-=1
+    if people[y-1]>0 and result[y-1]==1:
+        people[y-1]-=1
+        result[x-1]=1
+for x in range(n):
+    print(result[x],end="")
