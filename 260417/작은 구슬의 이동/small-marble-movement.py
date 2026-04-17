b31ebdd18e096f4f6e8e023355cda1c61d01260f
이윤우_구미_1반 <167078198+luster-woo@users.noreply.github.com>
2026-04-17 11:22:05 +0900

    Title: 작은 구슬의 이동, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 260417/작은 구슬의 이동/small-marble-movement.py
+++ 260417/작은 구슬의 이동/small-marble-movement.py
@@ -1,25 +1,30 @@
 n, t = map(int, input().split())
 r, c, d = input().split()
 r, c = int(r), int(c)
+
 dx = [0,1,0,-1]
 dy = [1,0,-1,0]
-i = 0
-while t>=0:
-    if d =="R":
-        i = 0
-    elif d=="D":
-        i =1
-    elif d=="L":
-        i=2
-    else:
-        i=3
-    ni = r+dx[i]
-    nj = c+dy[i]
-    if not(0<=ni<nj and 0<=ni<nj):
-        i = (i+2)%4
-        ni = r+dx[i]
-        nj = c+dy[i]
+
+# 🔥 처음에만 변환
+if d == "R":
+    i = 0
+elif d == "D":
+    i = 1
+elif d == "L":
+    i = 2
+else:
+    i = 3
+
+while t > -1:
+    ni = r + dx[i]
+    nj = c + dy[i]
+    if not (0 <= ni < n and 0 <= nj < n):
+        i = (i + 2) % 4
+        ni = r + dx[i]
+        nj = c + dy[i]
+
     r = ni
     c = nj
-    t-=1
-print(r,c)
+    t -= 1
+
+print(r, c)
