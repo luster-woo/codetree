d1e7ebdf8ac717fb9392acf11f13778953154c9f
이윤우_구미_1반 <167078198+luster-woo@users.noreply.github.com>
2026-04-17 11:25:24 +0900

    Title: 작은 구슬의 이동, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260417/작은 구슬의 이동/small-marble-movement.py
+++ 260417/작은 구슬의 이동/small-marble-movement.py
@@ -2,29 +2,32 @@ n, t = map(int, input().split())
 r, c, d = input().split()
 r, c = int(r), int(c)
 
-dx = [0,1,0,-1]
-dy = [1,0,-1,0]
+# 방향 정의: 상(U), 하(D), 우(R), 좌(L)
+# 문제의 R, C 좌표계에 맞게 행(dr), 열(dc) 변화량 설정
+mapper = {
+    'U': (-1, 0),
+    'D': (1, 0),
+    'R': (0, 1),
+    'L': (0, -1)
+}
 
-# 🔥 처음에만 변환
-if d == "R":
-    i = 0
-elif d == "D":
-    i = 1
-elif d == "L":
-    i = 2
-else:
-    i = 3
+# 반대 방향 전환용 매퍼
+opposite = {
+    'U': 'D', 'D': 'U', 'R': 'L', 'L': 'R'
+}
 
-while t >= 0:
-    ni = r + dx[i]
-    nj = c + dy[i]
-    if not (0 < ni <= n and 0 < nj <= n):
-        i = (i + 2) % 4
-        ni = r + dx[i]
-        nj = c + dy[i]
+dr, dc = mapper[d]
 
-    r = ni
-    c = nj
-    t -= 1
+for _ in range(t):
+    nr, nc = r + dr, c + dc
+    
+    # 격자 범위를 벗어나는지 확인
+    if 1 <= nr <= n and 1 <= nc <= n:
+        # 이동 가능한 경우: 위치 갱신
+        r, c = nr, nc
+    else:
+        # 이동 불가능한 경우 (벽): 방향만 반대로 전환 (1초 소모)
+        d = opposite[d]
+        dr, dc = mapper[d]
 
 print(r, c)
